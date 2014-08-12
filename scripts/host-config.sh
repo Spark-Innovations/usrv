f3908eedc32dff41dbd9b4385ff9c4b6c8061bc1
rongarret <ron@mickey>
2014-08-12 12:35:00 -0700

    Misc. bug fixes for host config script

--- scripts/host-config.sh
+++ scripts/host-config.sh
@@ -6,23 +6,21 @@ fi
 
 sudo hostname $1
 
-my_dir="$(dirname "$0")"
-config_files=$my_dir/../config_files
+config_files=~/usrv/config_files
 rcm_dir=/usr/local/lib/roundcubemail/skins/classic
 
 # Change host name in config files
 cd $config_files
 git checkout .
-sed -i 's/h1.usrv.us/$(hostname)/g' $config_files/*
-cd
+sed -i "s/h1.usrv.us/$(hostname)/g" $config_files/*
 
 sudo cp $config_files/usrv-home.html ~user-data/www/default/index.html
 
 sudo cp $config_files/usrv_logo.png $rcm_dir/images/roundcube_logo.png
 
 sudo cp $config_files/cloud.html ~user-data/www/default/
-sudo ln -s ~/caldavzap ~user-data/www/default/calendar
-sudo ln -s ~/carddavmate ~user-data/www/default/contacts
+sudo ln -s -f ~/caldavzap ~user-data/www/default/calendar
+sudo ln -s -f ~/carddavmate ~user-data/www/default/contacts
 cp $config_files/cdm-config.js ~/carddavmate/congig.js
 cp $config_files/cdz-config.js ~/caldavzap/congig.js
 
@@ -34,4 +32,6 @@ sudo service nginx reload
 # Start Radicale
 # NOTE: Radicale needs to be set up as a service, otherwise it
 # won't survive a reboot
-~/radicale/run.py
+echo Starting radicale
+cd ~/radicale
+./run.py
