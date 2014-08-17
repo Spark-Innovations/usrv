cfdfb6a9b2b3efde4a3f20cceb726541276cf78c
rongarret <ron@mickey>
2014-08-17 16:16:19 -0700

    Fix some bugs in ccl-setup.sh

--- scripts/ccl-setup.sh
+++ scripts/ccl-setup.sh
@@ -13,9 +13,10 @@ svn co http://svn.clozure.com/publicsvn/openmcl/release/1.8/linuxx86/ccl
 # Rebuild CCL image
 # cd ~/ccl
 # ./lx86cl64 -n -e "(progn (ccl:rebuild-ccl :full t) (quit))"
-# sudo ln -s -f /home/ron/ccl/lx86cl64 /usr/local/bin/ccl
+
+sudo ln -s -f /home/ron/ccl/lx86cl64 /usr/local/bin/ccl
 
 cd ~
-git clone git clone git@github.com:Spark-Innovations/ergolib.git
+git clone git@github.com:Spark-Innovations/ergolib.git
 ccl -l ~/usrv/install-quicklisp.lisp
 cp ~/usrv/ccl-init.lisp ~
