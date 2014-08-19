cfcad67ae78a08cb1974e5f10e8c350547c004ff
rongarret <ron@mickey>
2014-08-19 12:45:43 -0700

    Bug fixes for add-user script

--- scripts/add-user.sh
+++ scripts/add-user.sh
@@ -5,14 +5,14 @@ echo 'No user name specified'
 exit -1
 fi
 
-user = $1
+user=$1
 echo Setting up user $user
-pwd = $(pwgen -0 -A 12 1)
-ticket = $(pwgen -s 20 1)
+pwd=$(pwgen -0 -A 12 1)
+ticket=$(pwgen -s 20 1)
 echo $user:$pwd > ~/usrv/www/tickets/$ticket
 echo $user:$pwd >> ~/radicale/htpasswd
 cd ~/radicale/data
 mkdir $user
-cp $config_files/Calendar* $user/
-sudo ./tools/mail.py user add user1@$(hostname) passwd1
+cp ~/usrv/config_files/Calendar* $user/
+sudo ~/mailinabo/tools/mail.py user add ${user}@$(hostname) $pwd
 echo Ticket for $user is $ticket
